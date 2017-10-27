b2120301e276617977a247879983011b34bf3b8d
Pravenaditiyaa <kgnobel20@gmail.com>
2017-10-27 22:02:16 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './web-static/home/home.component';
 import { WebStaticComponent } from './web-static/web-static.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/static/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'static/home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'static/home', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
